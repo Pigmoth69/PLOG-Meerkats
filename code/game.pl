204c08045526eee74ae586c9f05e0a2418b9c848
JCB <pigmoth@gmail.com>
2015-11-05 16:36:15 +0000

    ainda falta cenas

--- code/game.pl
+++ code/game.pl
@@ -4,6 +4,8 @@ playGame(NumberPlayers):- 	availableColors(Colors),
 							N is 1,
 							assignPlayerColor(NumberPlayers, Info, Colors, N, RInfo).
 
+							
+
 
 
 assignPlayerColor(NumberPlayers, Info, Colors, N, RInfo):-
@@ -71,26 +73,107 @@ playerColorScreen(N, Color):-
 	write('***************************************************'), nl,
 	getEnter.
 
-play:- 
+play():- 
 	logicalBoard(LogicalBoard),
 	displayBoard(Board),
 	drawBoard(Board, LogicalBoard),
+	getEnter,
+	validMove(LogicalBoard,RowIdentifier,RowPos),
+	write('sai!'),nl,
+	abort,
+	moveStone(red,LogicalBoard,X,Y,ResultBoard),
+	write(ResultBoard),nl,
+	getEnter,
+	%-------------------------------------------------------
+	drawBoard(Board, ResultBoard),
 	getEnter.
+	
+							
 
 
+getPlayCoord(Message,RowIdentifier):-
+					getInteger(X),write(X),nl,nl,
+					(
+						RowIdentifier is X	,
+						X > 10 -> format('Invalid1 ~s !', [Message]),nl,getPlayCoord(Message,_);
+						X < 0 -> format('Invalid2 ~s !', [Message]),nl,getPlayCoord(Message,_)
+						
+					).		
+	
 
+getPlayCoord(_,1).
+getPlayCoord(_,2).
+getPlayCoord(_,3).
+getPlayCoord(_,4).
+getPlayCoord(_,5).
+getPlayCoord(_,6).
+getPlayCoord(_,7).
+getPlayCoord(_,8).
+getPlayCoord(_,9).				
+					
+	
+/*validMove(Board,RowIdentifier,RowPos):-
+	write('Insert the row identifier: '),nl,
+	getInteger(X),!,
+	(
+		X < 1 -> write('Invalid Row Identifier!'),nl,validMove(Board,RowIdentifier,RowPos);
+		X > 10 -> write('Invalid Row Identifier!'),nl,validMove(Board,RowIdentifier,RowPos);
+		write('Insert the row Position: '),nl,
+		
+		getInteger(Y),
+		(
+		Y < 1 -> write('Invalid Row Position!'),nl,validMove(Board,RowIdentifier,RowPos);
+		Y > 10 -> write('Invalid Row Position!'),nl,validMove(Board,RowIdentifier,RowPos)
+		),
+		
+	),RowIdentifier is X,RowPos is Y.*/
+	
+	
+moveStone(Color,LogicalBoard,RowIdentifier,RowPos,ResultBoard):-
+																getBoardRow(LogicalBoard,RowIdentifier,Row),
+																changeBoardRow(Row,RowPos,Color,ResRow),
+																changeBoard(LogicalBoard,RowIdentifier,ResRow,ResultBoard).
+changeBoard([],_,_,[]).
+changeBoard([H|A],NewRow,Elem,[H|NA]):- 
+	NewRow > 1,
+	Npos is NewRow-1,
+	changeBoard(A,Npos,Elem,NA).
+changeBoard([_|A],1,Elem,[Elem|A]). 													
+																
+changeBoardRow([],_,_,[]).
+changeBoardRow([H|A],RowPos,Elem,[H|NA]):- 
+	RowPos > 1,
+	Npos is RowPos-1,
+	changeBoardRow(A,Npos,Elem,NA).
+changeBoardRow([_|A],1,Elem,[Elem|A]). 	
 
+																
+getBoardRow([_|T],RowIdentifier,Row):-
+											RowIdentifier > 1,
+											N is RowIdentifier - 1,
+											getBoardRow(T,N,Row).
+getBoardRow([H|_],1,H).
+														
+	
+	
 
+/*
+moveStone(Color,[[H|T]|Tail],1,RowPos,ResultBoard):-moveStone(Color,[H|T],1,RowPos,ResultBoard).  
 
+moveStone(Color,[[H|T]|Tail],RowIdentifier,RowPos,ResultBoard):-
+											RowIdentifier > 1,
+											N is RowIdentifier - 1,
+											moveStone(Color,Tail,N,RowPos,ResultBoard).
+											
 
+moveStone(Color,[H|T],1,RowPos,[H|T]):-
+									N is RowPos - 1,
+									moveStone(Color,[H|T],0,N,T).
 
+moveStone(Color,[H|T],1,1,[Color|T]).
+										
 
-
-
-
-
-
-
+*/
 
 
 
@@ -142,7 +225,9 @@ horizontalBottomBorders([
 		'           ---------------------'
 	]).
 
-rowIdentifiers([' a         |', ' b       |', ' c     |', ' d   |', ' e |', ' f   |', ' g     |', ' h       |', ' i         |']).
+rowIdentifiers([' 1         |', ' 2       |', ' 3     |', ' 4   |', ' 5 |', ' 6   |', ' 7     |', ' 8       |', ' 9         |']).
+
+
 
 drawBoard(Board, LogicalBoard):- 
 			printTopBorder, nl,
@@ -171,4 +256,7 @@ printCell(Element, LogicalElement):-
 			LogicalElement = red     -> ansi_format([bold,fg(red)], ' ~w ', [Element]);
 			LogicalElement = green   -> ansi_format([bold,fg(green)], ' ~w ', [Element]).
 
+
 printTopBorder:- write('           ---------------------').
+
+

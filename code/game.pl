e404f54bb24293db02f67b77696d01b8325f59fa
guilhermevpinto <guilhermevpinto@gmail.com>
2015-11-06 04:29:16 +0000

    floodfill

--- code/game.pl
+++ code/game.pl
@@ -260,3 +260,144 @@ printCell(Element, LogicalElement):-
 printTopBorder:- write('           ---------------------').
 
 
+
+
+
+
+
+
+
+
+
+
+%------------------------------------%
+%-------------FloodFill--------------%
+%------------------------------------%
+
+registBoard([
+	            [0, 0, 0, 0, 0],
+	         [0, 0, 0, 0, 0, 0],
+	      [0, 0, 0, 0, 0, 0, 0],
+	   [0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0],
+	   [0, 0, 0, 0, 0, 0, 0, 0],
+	      [0, 0, 0, 0, 0, 0, 0],
+	         [0, 0, 0, 0, 0, 0],
+	            [0, 0, 0, 0, 0]
+	]).
+
+
+floodFill:-		logicalBoard(LogicalBoard),
+				flood(blue, Rb, LogicalBoard),
+				flood(red, Rr, LogicalBoard),
+				flood(green, Rg, LogicalBoard),
+				flood(yellow, Ry, LogicalBoard),
+				append([Rb], [Rr], R1), append([Rg], [Ry], R2), append(R1, R2, R), write(R), !.
+
+flood(Color, Result, LogicalBoard):- 	registBoard(RegistBoard),
+										searchNextColorOcurrence(1, 1, LogicalBoard, RegistBoard, Color, [Row | [Col | _]]),
+										searchColorGroups(Row, Col, Color, LogicalBoard, RegistBoard, R),
+										append([Color], [R], Result), !.
+
+searchColorGroups(9, 6, _, _, _, []).
+
+searchColorGroups(Row, Col, Color, LogicalBoard, RegistBoard, [FinalArea | Result]):- 	calculateArea(Row, Col, Color, RegistBoard, LogicalBoard, FinalArea, LastRegistBoard),
+																						searchNextColorOcurrence(Row, Col, LogicalBoard, LastRegistBoard, Color, [R | [C | _]]),
+																						searchColorGroups(R, C, Color, LogicalBoard, LastRegistBoard, Result).
+
+calculateArea(Row, Col, Color, RegistBoard, LogicalBoard, FinalArea, FinalRegistBoard):-validCell(Row, Col),
+																						visitedCell(Row, Col, RegistBoard, Visited),
+																						Visited = 0,
+																						checkForColor(Row, Col, NewColor, LogicalBoard),
+																						NewColor = Color,
+																						setVisitedCell(Row, Col, RegistBoard, NewRegistBoard),	
+																						MinorRow is Row - 1,
+																						MajorRow is Row + 1,
+																						MinorCol is Col - 1,
+																						MajorCol is Col + 1,
+																						calculateArea(MinorRow, MinorCol, Color, NewRegistBoard, LogicalBoard, A1, FR1),
+																						calculateArea(MinorRow, Col, Color, FR1, LogicalBoard, A2, FR2),
+																						calculateArea(MinorRow, MajorCol, Color, FR2, LogicalBoard, A3, FR3),
+																						calculateArea(Row, MinorCol, Color, FR3, LogicalBoard, A4, FR4),
+																						calculateArea(Row, MajorCol, Color, FR4, LogicalBoard, A5, FR5),
+																						calculateArea(MajorRow, MinorCol, Color, FR5, LogicalBoard, A6, FR6),
+																						calculateArea(MajorRow, Col, Color, FR6, LogicalBoard, A7, FR7),
+																						calculateArea(MajorRow, MajorCol, Color, FR7, LogicalBoard, A8, FinalRegistBoard),
+																						FinalArea is 1+A1+A2+A3+A4+A5+A6+A7+A8, !.
+
+
+calculateArea(_, _, _, RegistBoard, _, 0, RegistBoard).
+
+
+searchNextColorOcurrence(9, 6, _, _, _, [9, 6]).
+
+searchNextColorOcurrence(Row, Col, LogicalBoard, RegistBoard, Color, Result):-	not(validCell(Row, Col)),
+																				NewRow is Row+1,
+																				searchNextColorOcurrence(NewRow, 1, LogicalBoard, RegistBoard, Color, Result).
+																				
+
+searchNextColorOcurrence(Row, Col, LogicalBoard, RegistBoard, Color, Result):- 	search(Row, Col, LogicalBoard, Color, [R | [C | _]]),
+																				visitedCell(R, C, RegistBoard, 0),
+																				append([R], [C], Result).
+
+
+searchNextColorOcurrence(Row, Col, LogicalBoard, RegistBoard, Color, Result):- 	search(Row, Col, LogicalBoard, Color, [_]), 
+																				NextCol is Col + 1,
+																				searchNextColorOcurrence(Row, NextCol, LogicalBoard, RegistBoard, Color, Result).
+
+
+
+visitedCell(Row, Col, RegistBoard, Visited):-	getInfo(Row, Col, Visited, RegistBoard).
+visitedCell(9, 6, _, 0).
+
+setVisitedCell(Row, Col, RegistBoard, FinalRegistBoard):- setInfo(Row, Col, 1, RegistBoard, FinalRegistBoard).
+					
+checkForColor(Row, Col, Color, Board):- getInfo(Row, Col, Color, Board).
+
+
+
+%----------------------------------------------------------------------------%
+%-----Functions to get any info from a certain position of any matrix--------%
+%----------------------------------------------------------------------------%
+
+getInfo(0, 1, Head, [Head | _]).
+
+getInfo(0, Col, Info, [_ | Tail]):-	NewCol is Col - 1,
+									getInfo(0, NewCol, Info, Tail).
+
+getInfo(1, Col, Info, [Head | _]):- getInfo(0, Col, Info, Head).
+
+getInfo(Row, Col, Info, [_ | Tail]):- 	NextRow is Row - 1,
+										getInfo(NextRow, Col, Info, Tail).
+
+%----------------------------------------------------------------------------%
+%-----Functions to set any info into a certain position of any matrix--------%
+%----------------------------------------------------------------------------%
+
+setInfo(0, 1, Info, [_ | Tail],  [Info | Tail]).
+
+setInfo(0, Col, Info, [Head | Tail], [Head | RTail]):-	NewCol is Col - 1,
+											setInfo(0, NewCol, Info, Tail, RTail).
+
+setInfo(1, Col, Info, [Head | Tail], [RHead | Tail]):- setInfo(0, Col, Info, Head, RHead).
+
+setInfo(Row, Col, Info, [Head | Tail], [Head | RTail]):-	NewRow is Row - 1,
+															setInfo(NewRow, Col, Info, Tail, RTail), !.							
+
+%----------------------------------------------------------------------------%
+%-----Functions to search for a certain Element on a matrix------------------%
+%----------------------------------------------------------------------------%
+
+search(9, 6, _, _, [9, 6]).
+
+search(Row, Col, LogicalBoard, Element, [Row, Col]):-	validCell(Row, Col),
+														getInfo(Row, Col, NewElement, LogicalBoard),
+														Element = NewElement, !.
+
+search(Row, Col, LogicalBoard, Element, Result):- 	not(validCell(Row, Col)),
+													NewRow is Row + 1,
+													search(NewRow, 1, LogicalBoard, Element, Result).
+
+search(Row, Col, LogicalBoard, Element, Result):-	validCell(Row, Col),
+													NewCol is Col + 1,
+													search(Row, NewCol, LogicalBoard, Element, Result).
